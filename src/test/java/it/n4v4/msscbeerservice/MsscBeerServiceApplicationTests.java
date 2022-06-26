a01b7b034697220c31573f5b9c6490e36179e52c
Nicolò Vernocchi <niccolo.vernocchi@gmail.com>
2022-06-26 19:24:08 +0200

    Primo commit di base

---
+++ src/test/java/it/n4v4/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.n4v4.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
