bb91496df5a75c0bb695f49d9c2497b218f979e7
Nicolò Vernocchi <niccolo.vernocchi@gmail.com>
2022-08-19 21:45:22 +0200

    Aggiunta configurazione MapStruct

---
+++ src/main/java/it/n4v4/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,26 @@
+package it.n4v4.msscbeerservice.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<List> constraintViolationHandler(ConstraintViolationException e){
+		
+		List<String> errorsList = new ArrayList<>(e.getConstraintViolations().size());
+		
+		e.getConstraintViolations().forEach(constraintViolation -> errorsList.add(constraintViolation.toString()));
+		
+		return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+	}
+
+}
